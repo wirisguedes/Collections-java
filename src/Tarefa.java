a48517c56d901dee90a8bc66b760591b115f0a90
wirisguedes <wirisguedes@gmail.com>
2024-05-26 15:01:12 -0300

    implementacao ListaTarefas

---
+++ src/Tarefa.java
@@ -0,0 +1,12 @@
+public class Tarefa {
+
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
